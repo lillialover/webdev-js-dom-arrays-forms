a1d93c4acf3a17aa452610a792ea618a70ffa5b1
lillialover <kuzonmateusz60@gmail.com>
2023-11-17 10:37:55 +0000

    imagerotator

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.getElementById('myHeading').innerHTML = "Mateusz is cool";
 document.querySelector('nav ul li').setAttribute('class', 'currentPage');
+
+//colour changer
 let ColourButtons = document.querySelectorAll(".colPicker");
 for (let i = 0; i< ColourButtons.length; i++){
     ColourButtons[i].addEventListener("click", chgColour)
@@ -11,3 +13,20 @@ function chgColour(ev){
     let colourPicked = ev.target.classList[0] + "Back";
     bodyElement.setAttribute("class", colourPicked)
 }
+
+//image rotator
+let imageAr = [
+    'images/view1.jpg', 
+    'images/view2.jpg', 
+    'images/view3.jpg', 
+    'images/view4.jpg', 
+    'images/view5.jpg', 
+    'images/view6.jpg'
+]; 
+
+let imageCount = 0;
+setInterval(function(){
+    document.getElementById("myImages")
+    .setAttribute("src", imageAr[imageCount]);
+    imageCount++;
+}, 1000)
