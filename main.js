b26ed4c078be84639354c1fa22c23c23aaf70f3c
lillialover <kuzonmateusz60@gmail.com>
2023-11-17 09:57:40 +0000

    update

---
+++ main.js
@@ -0,0 +1,2 @@
+document.querySelector('nav ul li a').setAttribute('href', 
+'http://www.google.co.uk');
--- myScript.js
+++
@@ -1,4 +0,0 @@
-function init(){ 
-    console.info(document.getElementById("myHeading").innerHTML); 
-} 
-
